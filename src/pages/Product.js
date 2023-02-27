ee391e6fb38c09aa198a227b2e53033e9a8a3f8d
good_ssam <goodwebi@naver.com>
2023-02-27 21:21:09 +0900

    제품리스트 수정

--- src/pages/Product.js
+++ src/pages/Product.js
@@ -1,4 +1,11 @@
 
 export default function About() {
-    return <h1>제품리스트 화면</h1>
+    return (
+        <div>
+            <h1>제품리스트 화면</h1>
+            <div className="list_sw">
+                
+            </div>
+        </div>
+    )
 }
