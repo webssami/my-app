c86ceb506457a84a7662f205e4e47e7099328bfd
good_ssam <goodwebi@naver.com>
2023-02-28 03:09:16 +0900

    슬라이드 메인 수정

--- src/pages/Myslide.js
+++ src/pages/Myslide.js
@@ -1,8 +1,8 @@
-import React from 'react';
+import React, {useRef, useState} from 'react';
 
 // Import Swiper React components
 import {Swiper, SwiperSlide } from 'swiper/react';
-import {Pagination, Navigation} from 'swiper';
+import SwiperCore,{Pagination, Navigation, Autoplay} from 'swiper';
 
 import Slide1 from '../img/tan_main_img1.jpg';
 import Slide2 from '../img/tan_main_img2.jpg';
@@ -11,14 +11,26 @@ import 'swiper/css';
 import 'swiper/css/pagination'; //페이지블릿버튼
 import 'swiper/css/navigation'; //이전, 다음버튼
 
-export default () => {
+SwiperCore.use([Autoplay, Pagination])
+
+/* onMouseEnter={() => swiperRef.current.swiper.autoplay.stop()}
+    onMouseLeave={() => swiperRef.current.swiper.autoplay.start()} */
+export default function Myslide() {
+  const swiperRef = useRef(null)
+  const [btn, setBtn] = useState(false);
+  const posBtn = useRef(null);
   return (
     <div className='main_slide'>
+      
         <Swiper
-            modules={[Navigation, Pagination]}
+            ref={swiperRef}
+            modules={[Navigation, Pagination, Autoplay]}
             pagination = {{clickable:true}}
             navigation
             loop = {true}
+            autoplay = {{"delay":5000, "disableOnInteraction": false,
+            
+          }}
             spaceBetween={50}
             slidesPerView={1}
             onSlideChange={() => console.log('slide change')}
@@ -28,6 +40,18 @@ export default () => {
             <SwiperSlide><img src={Slide2} alt="slide2" /></SwiperSlide>
             
         </Swiper>
+        <button onClick={() => {
+          setBtn(!btn);
+          if(btn === false){
+            swiperRef.current.swiper.autoplay.stop();
+            posBtn.current.style.background = "#f40";
+            posBtn.current.innerText ="재생"
+          }else{
+            swiperRef.current.swiper.autoplay.start()
+            posBtn.current.style.background = "#ff0";
+            posBtn.current.innerText ="정지"
+          }
+        }} ref={posBtn}>정지</button>
     </div>
     
   );
--- src/pages/Product.js
+++ src/pages/Product.js
@@ -1,10 +1,67 @@
+import food01 from '../img/food/food1.jpg';
+import food02 from '../img/food/food2.jpg';
+import food03 from '../img/food/food3.jpg';
+import food04 from '../img/food/food4.jpg';
+import food05 from '../img/food/food5.jpg';
+import { Navigation, Pagination } from 'swiper';
 
-export default function About() {
+import { Swiper, SwiperSlide } from 'swiper/react';
+
+// Import Swiper styles
+import 'swiper/css';
+import 'swiper/css/navigation';
+import 'swiper/css/pagination';
+
+export default function Product() {
     return (
         <div>
             <h1>제품리스트 화면 swiper 4개씩</h1>
             <div className="list_sw">
-                
+              <Swiper
+                // install Swiper modules
+                modules={[Navigation, Pagination]}
+                spaceBetween={30}
+                slidesPerView={4}
+                loop = {true}
+                navigation
+                pagination={{ clickable: true }}
+                scrollbar={{ draggable: true }}
+                onSwiper={(swiper) => console.log(swiper)}
+                onSlideChange={() => console.log('slide change')}
+                >
+                <SwiperSlide>
+                    <img src={food01} alt="food01" />
+                    <div className='sl_text'>food01</div>
+                </SwiperSlide>
+                <SwiperSlide>
+                    <img src={food02} alt="food02" />
+                    <div className='sl_text'>food02</div>
+                </SwiperSlide>
+                <SwiperSlide>
+                    <img src={food03} alt="food03" />
+                    <div className='sl_text'>food03</div>
+                </SwiperSlide>
+                <SwiperSlide>
+                    <img src={food04} alt="food04" />
+                    <div className='sl_text'>food04</div>
+                </SwiperSlide>
+                <SwiperSlide>
+                    <img src={food05} alt="food05" />
+                    <div className='sl_text'>food05</div>
+                </SwiperSlide>
+                <SwiperSlide>
+                    <img src={food03} alt="food03" />
+                    <div className='sl_text'>food03</div>
+                </SwiperSlide>
+                <SwiperSlide>
+                    <img src={food04} alt="food04" />
+                    <div className='sl_text'>food04</div>
+                </SwiperSlide>
+                <SwiperSlide>
+                    <img src={food05} alt="food05" />
+                    <div className='sl_text'>food05</div>
+                </SwiperSlide>
+                </Swiper>
             </div>
         </div>
     )
