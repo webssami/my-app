122051f091f04d9b9ba1ff4537e58960fabe52ec
good_ssam <goodwebi@naver.com>
2023-02-27 23:46:23 +0900

    제품리스트 슬라이드

--- src/pages/Product.js
+++ src/pages/Product.js
@@ -2,7 +2,7 @@
 export default function About() {
     return (
         <div>
-            <h1>제품리스트 화면</h1>
+            <h1>제품리스트 화면 swiper 4개씩</h1>
             <div className="list_sw">
                 
             </div>
